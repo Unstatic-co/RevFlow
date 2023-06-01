0f449b45b402c948ba024c20a7c9c2c1112f58d2
Peter Vu <tiep.vuvan614@gmail.com>
2023-06-01 17:52:50 +0700

    Added StoreKit transaction event listener

--- packages/webhook-server/src/index.ts
+++ packages/webhook-server/src/index.ts
@@ -1,22 +1,23 @@
 // an express app
 import express from 'express'
 import { registerAppInstallation } from './controllers'
+import { storekitCallbackHandler } from './store-kit'
 
 const app = express()
 const port = process.env.PORT || 3694
 
 app.use(express.json())
 
-app.post('/api/v1/storekit-callback')
-app.post('/api/v1/google-play-callback')
+app.post('/storekit-callback', storekitCallbackHandler)
+app.post('/google-play-callback')
 
 // register an app installation
-app.post('/api/v1/installations', registerAppInstallation)
+app.post('/installations', registerAppInstallation)
 
-app.post('/api/v1/installations/:installationId/attributes')
+app.post('/installations/:installationId/attributes')
 
 // link installation to an existing customer
-app.post('/api/v1/installations/:installationId/link')
+app.post('/installations/:installationId/link')
 
 app.listen(port, () => {
     console.log(`🚀 Server started on port ${port}. http://localhost:${port}`)
